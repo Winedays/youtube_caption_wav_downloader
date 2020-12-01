0c38effbcafae708812bb8e5e5b2a19da5890488
Winedays <adler011014@gmail.com>
2020-12-01 11:39:43 +0800

    change class structure
    change class name
    set url in download method, not constructor
    format comment

--- downloader.py
+++ downloader.py
@@ -1,28 +1,40 @@
 import os
-import json
 import subprocess
 import sys
 from io import BytesIO
 from pytube import YouTube
 
 # give a youtube video url , download it caption & audio as a file
-class ytDownload :
+class YoutubeDownload :
     # set url & files save location
-    # url : youtube link url
-    # savePath : save file path 
-    def __init__(self, url, savePath: str = "./") :
+    # @parms savePath, save file path, optional, defaults to run path.
+    def __init__(self, savePath: str = "./") :
         if not self.checkPath( savePath ) :
             raise Exception(f"Save Path Error : save path {savePath} not exist.") 
-        self.url = url
+        self.url = ""
         self.savePath = savePath
-        self.yt = YouTube(url)
+        self.yt = None
         return ;
     
+    # get YouTube type object
+    # @parms url, youtube link url.
+    # @return   youtube type object
+    def getVideoInfo( self , url ) :
+        # check if this url already loaded
+        if self.url != url :
+            self.yt = YouTube(url)
+            self.url = url
+        return self.yt ;
+    
     # get captions and save as srt/xml format file
-    # fileName : save file name without extension
-    # language : language of captions you want to get
-    # fileType : save file format, should be "srt" or "xml"
-    def download_captions( self , fileName: str = None , language: str = "zh-Hant" , fileType: str = "srt" ) :
+    # @parms url, youtube link url.
+    # @parms fileName, save file name without extension, optional, defaults to video title.
+    # @parms language, language of captions you want to get, optional, defaults to "zh-Hant".
+    # @parms fileType, save file format, should be "srt" or "xml", optional, defaults to "srt".
+    # @return   save path of caption file
+    def download_captions( self , url, fileName: str = None , language: str = "zh-Hant" , fileType: str = "srt" ) :
+        # check if this url already loaded
+        self.getVideoInfo( url )
         # check parms.
         if language not in self.yt.captions :
             print( f"{sys.argv[0]}: {self.yt.watch_url} do not have {language} caption." )
@@ -44,13 +56,19 @@ class ytDownload :
         
         print( f"{sys.argv[0]}: {self.yt.watch_url} caption save as {saveFile}." )
         return saveFile ;
-        
+    
     # extract audio from video and save as a wav format file 
-    # fileName : save file name without extension
-    # keepMp4 : is need to keep the mp4 audio file 
-    def download_wav( self , fileName: str = None, keepMp4: bool = False) :
+    # @parms url, youtube link url.
+    # @parms fileName, save file name without extension, optional, defaults to video title.
+    # @parms keepMp4, is need to keep the mp4 audio file , optional, defaults to False.
+    # @return   save path of wav file
+    def download_wav( self , url, fileName: str = None, keepMp4: bool = False) :
+        # check if this url already loaded 
+        self.getVideoInfo( url )
+        # check parms.
         if not fileName :
             fileName = self.yt.title
+            
         # get audio stream
         stream = self.yt.streams.get_audio_only()
         buffer = BytesIO()
@@ -72,23 +90,23 @@ class ytDownload :
         return wavFile ;
     
     # convert a mp4 file to wav format by ffmpeg
-    # infile : input file position
-    # outfile : output file position
+    # @parms infile, input file position.
+    # @parms outfile, output file position.
     def convert_mp4_wav( self , infile: str , outfile: str ) :
         command = f"ffmpeg -y -i \'{infile}\' -ac 1 -ar 16000 -f wav \'{outfile}\'"
         subprocess.run(command, shell=True)
         return ;
     
     # check folder path exist
-    # folder : path of folder need to check
+    # @parms folder, path of folder need to check
     def checkPath( self , folder ) :
         if os.path.isdir( folder ) :
             return True ;
         else :
             return False ;
-
+    
     # check file exist
-    # file : path of file need to check
+    # @parms file, path of file need to check
     def checkFile( self , file ) :
         if os.path.isfile( file ) :
             return True ;
@@ -99,9 +117,9 @@ class ytDownload :
 if __name__ == "__main__" :  
     try :
         url = 'https://youtube.com/watch?v=4BOzM0lYjMw'
-        downloader = ytDownload( url , savePath = "./tmp" )
-        downloader.download_captions( "caption" , fileType = "srt" )
-        downloader.download_wav( "audio" )    
+        downloader = YoutubeDownload( savePath = "./tmp" )
+        downloader.download_captions( url , "caption" , fileType = "srt" )
+        downloader.download_wav( url , "audio" )    
     except Exception as e :
         print(e)
         # raise Exception("Exception Error as "+e)
